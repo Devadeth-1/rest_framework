9affa4b4a52e4c9f7041bfa4aba2a9b3246c9acb
Devadeth shaji <devadethofficial@gmail.com>
2025-04-11 11:28:54 +0530

    Some Updations have been done

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from study.views import index
+from study.views import index, student
 urlpatterns = [
     path('index/', index, name='index'),
+    path('study/', student,name='Students'),
 ]
--- study/views.py
+++ study/views.py
@@ -1,11 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from study.models import Students
+from study.serializer import StudentSerializer
 
 
-
-
-@api_view(['GET','POST'])
+@api_view(['GET'])
 def index(request):
 
     students_details = {
@@ -15,3 +15,41 @@ def index(request):
     }
 
     return Response(students_details)
+
+
+
+
+@api_view(['GET'], ['POST'], ['PUT'], ['PATCH'], ['DELETE'])
+def student(request):
+    if request.method == 'GET':
+        objStudents = Students.objects.all()
+        serializer = StudentSerializer(objStudents, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = request.data
+        serializer = StudentSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+    elif request.method == 'PUT':
+        data = request.data
+        obj = Students.objects.get(id = data['id'])
+        serializer = StudentSerializer(obj, data = data, partial = False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+    elif request.method == 'PATCH':
+        data = request.data
+        obj = Students.objects.get(id = data['id'])
+        serializer = StudentSerializer(obj, data = data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.error)
+    else:
+        data = request.data
+        obj = Students.objects.get(id = data['id'])
+        obj.delete()
+        return Response({'message': 'Student deleted'})
