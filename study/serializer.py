d91b6d82549258db31cc3cb51ac3ec7cf3f01618
Devadeth shaji <devadethofficial@gmail.com>
2025-04-11 11:41:27 +0530

    Some Updations have been done

--- study/serializer.py
+++ study/serializer.py
@@ -5,3 +5,14 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Students
         fields = '__all__'
+
+    def validate(self, data):
+        spl_chars = "!@#$%^&*()-+?_=,<>/"
+        
+        if any(c in spl_chars for c in data['name']):
+            raise serializers.ValidationError("Name should not have special characters")
+        
+        if data['age'] < 18:
+            raise serializers.ValidationError("Age should not be less than 18")
+        
+        return data
